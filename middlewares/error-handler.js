6ccda73afe8da0b3b594e5794a8cc53ab5352316
Sanc <ienkhbr@outlook.com>
2025-05-14 16:43:11 -0700

    added next

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,4 +1,4 @@
-module.exports = (err, req, res,) => {
+module.exports = (err, req, res,next) => {
   console.error(err.stack);
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Something went wrong';
