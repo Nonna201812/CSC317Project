34f98ef7f9a27da801a4156fa65b94ef6bbf9766
Alondra Sanchez <nonna201812@gmail.com>
2025-05-15 12:40:00 -0700

    Update budget.js

--- routes/budget.js
+++ routes/budget.js
@@ -5,7 +5,7 @@
 const express = require('express');
 const router = express.Router();
 const { isAuthenticated } = require('../middlewares/auth');
-const { setLimit, getLimit } = require('../controllers/transactionController'); //remove if it doesn't work
+const { setLimit, getLimit } = require('../controllers/transactionController'); 
 
 // GET /budget - Budget planner page
 router.get('/budget', isAuthenticated, (req, res) => {
@@ -15,5 +15,5 @@ router.get('/budget', isAuthenticated, (req, res) => {
         isAuthenticated: !!req.session.user
     });
 });
-router.post('/budget', isAuthenticated, setLimit); //remove if it doesn't work
+router.post('/budget', isAuthenticated, setLimit); 
 module.exports = router;
